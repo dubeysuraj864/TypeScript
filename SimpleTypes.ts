018f8619ffd1ea4d8fd6d2272c537e18732a0b06
Suraj Dubey <dubeysuraj864@gmail.com>
2023-08-31 00:13:41 +0530

    pushing

---
+++ SimpleTypes.ts
@@ -0,0 +1,18 @@
+// Explicit Type
+let firstName: string = "typescript";
+console.log(firstName);
+
+// Implicit Type
+let firstName = "typescript";
+console.log(firstName);
+
+// let firstName = "Coffee";
+// firstName = 33;
+
+// it will throw error if data types do not match
+
+// boolean - true or false values
+// number - whole numbers and floating point values
+// string - text values like "typescript rocks"
+// bigint - whole num for floating point values, but allows larger -ve and +ve numbers.
+// symbol - are used to create a globally unique identifier
