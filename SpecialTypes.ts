fb0d2c7da86324b49117b5896f6acb320eb7b0d5
Suraj Dubey <dubeysuraj864@gmail.com>
2023-08-31 11:43:23 +0530

    st

--- SpecialTypes.ts
+++ SpecialTypes.ts
@@ -6,8 +6,29 @@
 
 // fix
 
-let u: any = true;
-u = "string";
-console.log(u);
-console.log(Math.round(u));
+// let u: any = true;
+// u = "string";
+// console.log(u);
+// console.log(Math.round(u));
 
+// type unknown 
+//unknown is similar to any but safer alternative to any
+
+let w: unknown  = 1;
+w = "String";
+w = {
+    runANonExistedMethod: () => {
+        console.log("I think therefore I am");
+    }
+} as {runANonExistedMethod: ()=> void}
+
+if(typeof w=== 'object' && w!== null){
+    (w as { runANonExistedMethod : Function}).runANonExistedMethod();
+}
+
+//
+
+let x: never = true; //let x: never = true;
+let y: undefined = undefined;
+let z: null = null;
+//undefined and null are types that refer to the JavaScript primitives undefined and null respectively.
