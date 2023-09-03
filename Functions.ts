81056f4b9255d06cfde0dc592625fb3d08a5c5b3
Suraj Dubey <dubeysuraj864@gmail.com>
2023-09-03 14:12:44 +0530

    ts functions

---
+++ Functions.ts
@@ -0,0 +1,42 @@
+
+// function getTime(): number{
+//     return new Date().getTime();
+// }
+
+// console.log(getTime())
+
+// console.log(printHello())
+
+// function printHello(): void {
+//     console.log('Hello!');
+//   }
+
+
+// function multiply (a:number, b:number){
+//     return a*b;
+// }
+// console.log(
+//     multiply(23,2))
+
+// the `?` operator here marks parameter `c` as optional
+// function add(a: number, b: number, c?: number) {
+//     return a + b + (c || 0);
+//   }
+
+// console.log(  add(3,4,5))
+
+// default parameter 
+
+// function pow(value: number, exponent: number = 10) {
+//     return value ** exponent;
+//   }
+
+//   console.log(pow(2,3));
+
+// function divide ({dividend, divisor}: {dividend: number, divisor: number}){
+//     return dividend / divisor;
+// }
+
+// function add(a: number, b: number, ...rest: number[]){
+//     return a+b+rest.reduce((p,c)=> p+c,0);
+// }
